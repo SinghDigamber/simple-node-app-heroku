5b575fb3a1872621906905d0efacd39322268208
SinghDigamber <digambersingh126@gmail.com>
2019-06-18 00:41:21 +0530

    new release

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 4000, function () {
     console.log('Node app is working!');
 });
